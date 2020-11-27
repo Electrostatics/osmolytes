acb0d0bcbf97506813bde602416b08f9423d26fe
Nathan Baker <nathanandrewbaker@gmail.com>
2020-11-26 17:51:49 -0800

    Update README with PyPI link.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "This code attempts to predict the influence of osmolytes on protein "
         "stability, using the methods in:  Auton M, Bolen DW. Predicting the "
         "energetics of osmolyte-induced protein folding/unfolding. "
-        "_Proc Natl Acad Sci_ 102:15065 (2005) "
+        "*Proc Natl Acad Sci* 102:15065 (2005) "
         "https://doi.org/10.1073/pnas.0507053102. Other models may be added "
         "in the future."
     ),
