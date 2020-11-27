ba1d7f1239cdbdf3a020b2ad09d8e67f946309f5
Nathan Baker <nathanandrewbaker@gmail.com>
2020-11-26 17:47:17 -0800

    Fix the fix to the description.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=(
         "This code attempts to predict the influence of osmolytes on protein "
         "stability, using the methods in:  Auton M, Bolen DW. Predicting the "
-        "energetics of osmolyte-induced protein folding/unfolding.
+        "energetics of osmolyte-induced protein folding/unfolding. "
         "_Proc Natl Acad Sci_ 102:15065 (2005) "
         "https://doi.org/10.1073/pnas.0507053102. Other models may be added "
         "in the future."
